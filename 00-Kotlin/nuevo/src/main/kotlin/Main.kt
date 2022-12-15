396b13a203a1db3f493f4b16de47d3609e4fba04
Byron Salvador <byron.salvador@epn.edu.ec>
2022-12-15 09:44:43 -0500

    adicion en Firebase de la app com.example.blsg_app

--- 00-Kotlin/nuevo/src/main/kotlin/Main.kt
+++ 00-Kotlin/nuevo/src/main/kotlin/Main.kt
@@ -32,8 +32,8 @@ fun main() {
 
     //Condicionales
     if (true) {
-    } else{
-            println("hola")
+    } else {
+        println("hola")
     }
 
 
@@ -54,10 +54,10 @@ fun main() {
 
     val coqueteo = if (estadoCivilWhen == "S") "SI" else "NO"
 
-    val sumaUno = Suma(  1,  1);
-    val sumaDos = Suma(  null,  1);
-    val  sumaTres = Suma(  1,  null);
-    val  sumaCuatro = Suma(  null,  null);
+    val sumaUno = Suma(1, 1);
+    val sumaDos = Suma(null, 1);
+    val sumaTres = Suma(1, null);
+    val sumaCuatro = Suma(null, null);
 
     sumaUno.sumar()
     sumaDos.sumar()
@@ -66,13 +66,12 @@ fun main() {
     sumaCuatro.sumar()
 
     Suma.pi
-    Suma.elevarAlCuadrado( 2)
+    Suma.elevarAlCuadrado(2)
     Suma.historialsumas
 
 
-
 // Arreglo Estatico
-    val arregloEstatico: Array<Int> = arrayOf<Int>(1,2,3)
+    val arregloEstatico: Array<Int> = arrayOf<Int>(1, 2, 3)
     println(arregloEstatico)
 
 // Avregto Dinamicos
@@ -82,26 +81,29 @@ fun main() {
     arregloDinamico.add(12)
     println(arregloDinamico)
 
-// Operadores -> Sirven para 1s anregtos estéticos y dindxicos
-
+// ***************Operadores -> Sirven para arreglos estáticos y dinámicos********
 
 
 // FOR EACH -> Unit
 // Iterar un arreglo
+    println("-------------------Operador FOR EACH-------------------------")
 
     val respuestaForEach: Unit = arregloDinamico
-        .forEach{
-            valorActual: Int ->
+        .forEach { valorActual: Int ->
             println("Valor actual:  ${valorActual}")
         }
-   arregloEstatico
-       .forEachIndexed { indice: Int, valorActual: Int ->
-           println("Valor ${valorActual} Indice: ${indice}")
+    arregloEstatico
+        .forEachIndexed { indice: Int, valorActual: Int ->
+            println("Valor ${valorActual} Indice: ${indice}")
 
-       }
+        }
     println(respuestaForEach)
 
+    println("---------------------------------------------------------")
 
+
+
+    println("-------------------Operador MAP-------------------------")
 
 // MAP -> Muta el arregto (Cambio el arreglo)
 // 1) Enviemos el nuevo valor de la iteracion
@@ -109,21 +111,87 @@ fun main() {
 
     val respuestaMap: List<Double> = arregloDinamico
         .map { valorActual: Int ->
-            return@map valorActual . toDouble () + 108.60
+            return@map valorActual.toDouble() + 108.60
         }
-
     println(respuestaMap)
 
-    val respuestaMapDos = arregloDinamico.map {it + 15}
-        .map{valorActual: Int ->
+    val respuestaMapDos = arregloDinamico.map { it + 15 }
+        .map { valorActual: Int ->
             return@map valorActual + 15
         }
 
     println(respuestaMapDos)
 
+    println("---------------------------------------------------------")
 
 
-            //Funciones
+    println("-------------------Operador FILTER-------------------------")
+
+// Filter -> FILTRAR EL ARREGLO
+// 1) Devolver una expresion (TRUE 0 FALSE)
+// 2) Nuevo arreglo filtrado
+
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter { valorActual: Int ->
+            val mayoresACinco: Boolean = valorActual > 5// Expresion Condicion
+            return@filter mayoresACinco
+        }
+
+    val respuestaFilterDos = arregloDinamico.filter { it <= 5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+    println("---------------------------------------------------------")
+
+
+
+    println("-------------------Operador OR -- AND-------------------------")
+
+    //OR AND
+    //OR -> ANY (ALGUNO CUMPLE?)
+    //AND -> ALL (TODOS CUMPLEN)
+
+    val respuestaAny: Boolean = arregloDinamico
+        .any { valorActual: Int ->
+            return@any (valorActual > 5)
+        }
+    println(respuestaAny) //true
+
+    val respuestaALL: Boolean = arregloDinamico
+        .all { valorActual: Int ->
+            return@all (valorActual > 5)
+        }
+    println(respuestaALL) //false
+
+    println("---------------------------------------------------------")
+
+
+
+    println("-------------------Operador REDUCE-------------------------")
+
+    // REDUCE -> Valor acumulado
+    // Valor acumulado = 0 (Siempre 0 en Lenguaje Kotlin)
+    // [1, 2, 3, 4, 5] -> Sumeme todos los valores del arreglo
+    //valorIteracion1 = valorEmpieza + 1 = 0+1 -> = 1 -> Iteracion 1
+    //valorIteracion2 = valorIteracion1 + 2 = 1+ 2 = 3 -> Iteracion 2
+    //valorIteracion3 = valorIteracion2 + 3 = 3 + 3 = 6 -> Iteracion 3
+    //valorIteracion4 = valorIteracion3 + 4 = 6 + 4 = 10 -> Iteracion 4
+    //valorIteracion5 = valorIteracion4 + 5 = 10 + 5 = 15 -> Iteracion 5
+
+
+    val respuestaReduce: Int = arregloDinamico
+        .reduce { //acumulado (contador) = 0 -> siempre empieza en 0
+                acumulado: Int, valorActual: Int ->
+            return@reduce (acumulado + valorActual) // -> Logica del negocio
+        }
+    println(respuestaReduce) //78
+
+    println("---------------------------------------------------------")
+
+
+    //************************************************************************************
+
+
+    //Funciones
     //void imprimirNombre(String nombre){}
     //Unit == void
 
@@ -131,13 +199,13 @@ fun main() {
         println("nombre:  ${nombre}");
     }
 
-    fun calcularSueldo(sueldo: Double, tasa: Double = 12.00, bonoEspecial: Double? = null, ): Double {
+    fun calcularSueldo(sueldo: Double, tasa: Double = 12.00, bonoEspecial: Double? = null): Double {
         //String -> String?
         //Int -> Int?
         //Date -> Date?
-        if (bonoEspecial == null){
+        if (bonoEspecial == null) {
             return sueldo * (100 / tasa)
-        }else{
+        } else {
             return sueldo * (100 / tasa) + bonoEspecial
         }
     }
@@ -145,12 +213,12 @@ fun main() {
 }
 
 //Creacion de clases
-abstract class NumerosJava{
+abstract class NumerosJava {
     protected val numeroUno: Int;
     private val numeroDos: Int;
 
 
-    constructor(uno: Int, dos: Int){
+    constructor(uno: Int, dos: Int) {
         //Bloque codigo constructor
         this.numeroUno = uno;
         this.numeroDos = dos;
@@ -163,13 +231,13 @@ abstract class Numeros( // Constructor Primario
 //uno: Int, // Parametro
 //public var uno: Int, // Propiedad de la clase publica
 //var uno: Int, // Propiedad de la clase publica
-protected val numeroUno: Int, // Propiedad de La clase protected
-protected val numeroDos: Int // Propiedad de la clase protected
+    protected val numeroUno: Int, // Propiedad de La clase protected
+    protected val numeroDos: Int // Propiedad de la clase protected
 ) {
 
-  //  protected val numeroUno: Int
-  //  var cedula: String = "";
-  //  public var cedula: String = "";
+    //  protected val numeroUno: Int
+    //  var cedula: String = "";
+    //  public var cedula: String = "";
 
     init { // Bloque codigo constructor PRIMARIO
         //this.numeroUno = uno
@@ -182,12 +250,11 @@ protected val numeroDos: Int // Propiedad de la clase protected
 }
 
 
-
 //Nuleable en el primer parametro
 class Suma( // Constructor Primario Suma
     uno: Int, // Parametro
     dos: Int // Parametro
-): Numeros(uno, dos) {
+) : Numeros(uno, dos) {
     init { // Bloque constructor primario
         this.numeroUno
         this.numeroDos
@@ -234,6 +301,7 @@ class Suma( // Constructor Primario Suma
         fun elevarAlCuadrado(num: Int): Int {
             return num * num
         }
+
         val historialsumas = arrayListOf<Int>()
         fun agregarHistorial(valorNuevaSuma: Int) {
             historialsumas.add(valorNuevaSuma)
