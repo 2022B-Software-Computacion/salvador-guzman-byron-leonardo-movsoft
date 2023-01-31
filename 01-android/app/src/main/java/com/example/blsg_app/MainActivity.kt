7b210fe4fbd63d37a2373f19a6c9d433eb67911e
Byron Salvador <byron.salvador@epn.edu.ec>
2023-01-31 09:48:45 -0500

    Aplicacion ejecutandose hasta GRecyclerView

--- 01-android/app/src/main/java/com/example/blsg_app/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/blsg_app/MainActivity.kt
@@ -55,6 +55,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Base de datos sqlite
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
+
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
             .setOnClickListener {
@@ -81,6 +85,18 @@ class MainActivity : AppCompatActivity() {
         botonIntent
             .setOnClickListener {
                 abrirActividadConParametros(CIntentExplicitoParametros::class.java)
+            }
+
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
+
+        val botonRView = findViewById<Button>(R.id.btn_revcycler_view)
+        botonRView
+            .setOnClickListener {
+                irActividad(GRecyclerView::class.java)
             }
     }
 
