f4b136e1454201d43bd29d318fb6bd8f659ea6ee
Byron Salvador <byron.salvador@epn.edu.ec>
2023-02-21 20:18:07 -0500

    Aplicación funcionando con carga de imágenes

--- MessengerAppRecyclerView/app/src/main/java/com/example/messengerapprecyclerview/MessengerUsuarios.kt
+++ MessengerAppRecyclerView/app/src/main/java/com/example/messengerapprecyclerview/MessengerUsuarios.kt
@@ -8,105 +8,133 @@ class MessengerUsuarios {
                 "Vas a ir a clases mañana?",
                 "21 feb.",
                 "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Anahí Vazques",
                 "Vas a ir a clases mañana?",
                 "21 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Daniel Salvador",
                 "Comprarás el pan",
                 "18 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Byron Salvador",
                 "Ya llegó?",
                 "22 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Mateus Vaca",
                 "Cfm ese basket",
                 "19 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Alexis Vizuete",
                 "Vas a ir a clases mañana?",
                 "21 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Anahí Vazques",
                 "Vas a ir a clases mañana?",
                 "21 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Daniel Salvador",
                 "Comprarás el pan",
                 "18 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Byron Salvador",
                 "Ya llegó?",
                 "22 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Mateus Vaca",
                 "Cfm ese basket",
                 "19 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Alexis Vizuete",
                 "Vas a ir a clases mañana?",
                 "21 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Anahí Vazques",
                 "Vas a ir a clases mañana?",
                 "21 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Daniel Salvador",
                 "Comprarás el pan",
                 "18 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Byron Salvador",
                 "Ya llegó?",
                 "22 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
             Messenger(
                 "Mateus Vaca",
                 "Cfm ese basket",
                 "19 feb.",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA",
-                "https://images.app.goo.gl/8soJPxVwf2bZVjnQA"
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
+            ),
+            Messenger(
+                "Anahí Vazques",
+                "Vas a ir a clases mañana?",
+                "21 feb.",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
+            ),
+            Messenger(
+                "Daniel Salvador",
+                "Comprarás el pan",
+                "18 feb.",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
+            ),
+            Messenger(
+                "Byron Salvador",
+                "Ya llegó?",
+                "22 feb.",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
+            ),
+            Messenger(
+                "Mateus Vaca",
+                "Cfm ese basket",
+                "19 feb.",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png",
+                "https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
             ),
         )
     }
